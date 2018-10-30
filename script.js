53a3cd5c3c74d81497e4b59b59b8830d2468a7ba
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-10-30 10:07:39 +0100

    ex 6

---
+++ script.js
@@ -0,0 +1,13 @@
+// // var myapp = angular.module("SampleApp", []);
+// // myapp.controller("VoitureCntrl", function ($scope) {
+// //     $scope.voitures =
+// //
+// // });
+//
+// var App = angular.module('testApp', []);
+// App.controller('VoitureCntrl', function($scope, $http) {
+//   $http.get('voiture.json')
+//        .then(function(res){
+//           $scope.voitures = res.data;
+//         });
+// });
